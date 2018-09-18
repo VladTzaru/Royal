af98c35f9ddfdf2cf312ed551e937834de2c05c4
VladTzaru <bvanja.87@gmail.com>
2018-09-18 21:46:58 +0200

    add router transition

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
     <MainNavigation/>
-    <router-view></router-view>
+    <transition name="fade" mode="out-in">
+      <router-view></router-view>
+    </transition>
     <Footer/>
   </div>
 </template>
@@ -19,97 +21,9 @@ export default {
 
 <style lang="scss">
 @import './assets/scss/settings';
-
-body {
-  margin: 0;
-  padding: 0;
-  color: $text-primary;
-  text-align: left;
-  background-color: #fff;
-  font-weight: 400;
-  letter-spacing: 0;
-  -webkit-font-smoothing: antialiased;
-  text-transform: none;
-  font-family: 'Roboto', sans-serif;
-  font-size: 16px;
-  line-height: 26px;
-  overflow-x: hidden;
-}
-
-.h1, .h2, .h3, .h4, .h5, .h6, h1, h2, h3, h4, h5, h6 {
-  margin-bottom: 1rem;
-  font-weight: 500;
-  color: inherit;
-}
-
-a {
-  transition: $transition-primary;
-  text-decoration: none !important;
-  color: $text-primary;
-  &:hover {
-    color: $primary;
-  }
-}
-
-.bg-skew {
-  position: relative;
-  &:before {
-    content: "";
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    transform: skewY(-5deg);
-    transform-origin: top left;
-    z-index: -1;
-  }
-}
-
-.bg-skew-light:before {
-    background-color: $secondary-light;
-}
-
-.btn {
-  font-size: 16px;
-  outline: none !important;
-  letter-spacing: .125rem;
-  text-transform: uppercase;
-}
-
-.btn-primary {
-  transition: $transition-primary;
-  color: #fff;
-  padding: .875rem 1.5rem;
-  font-weight: 700;
-  background-color: $primary-light;
-  border: $primary-light;
-  border-color: $primary-light;
-  box-shadow: $box-shadow-primary;
-  &:hover, &:focus, &:active, &:visited {
-    background-color: $primary !important;
-    box-shadow: $box-shadow-hover;
-    outline: none !important;
-  }
-}
-
-.hr-rule {
-  width: 3rem;
-  border-top-width: 2px !important;
-  margin-bottom: 46px;
-}
-
-.border-secondary {
-  border-top-color: $primary !important;
-}
-
-.btn-pill {
-  border-radius: 10rem;
-}
-
-.img-thumbnail {
-    box-shadow: $box-shadow-primary;
-}
+@import './assets/scss/base';
+@import './assets/scss/transitions';
+@import './assets/scss/helpers';
 </style>
 
 
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,23 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+<main>
+  <Hero>
+    <h1 slot="hero-title" class="display-4">Brza, jednostavna i povoljna rešenja</h1>
+    <p slot="hero-text" class="lead mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus sed
+      perspiciatis eius officia quisquam fugit ad a repellendus.</p>
+    <router-link slot="hero-btn" class="btn btn-primary btn-pill" to="/usluge" tag="button">Saznaj više</router-link>
+    <img slot="hero-img" src="../assets/img/wireless.jpg" class="d-none d-lg-block img-fluid img-thumbnail" alt="Creation process">
+  </Hero>
+</main>
+
 </template>
+
+<script>
+
+import Hero from '../components/Hero.vue'
+export default {
+  name: 'Home',
+  components: {
+    Hero
+  }
+}
+</script>
