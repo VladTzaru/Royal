260ae2aba45d276213f74dac3c92011f9315c4aa
VladTzaru <bvanja.87@gmail.com>
2018-09-19 19:05:14 +0200

    add contact component

--- src/components/base/Contact.vue
+++ src/components/base/Contact.vue
@@ -1,32 +1,36 @@
 <template>
-    <main id="content">
+    <main>
         <div class="container">
-            <h1 class="display-4 mb-5 py-5 text-center">Contact</h1>
+            <h1 class="display-4 mb-5 py-5 text-center">Kontakt</h1>
         </div>
         <div class="bg-skew bg-skew-light">
             <div class="container">
                 <div class="row justify-content-lg-center">
                     <div class="col-lg-9">
-                        <p>Please fill out the form below and we will get back to you as soon as possible.</p>
-                        <p class="small">Fields marked with an * are required.</p>
+                        <p>Molimo popunite formular ispod i javićemo vam se što pre.</p>
+                        <p class="small">Polja označena sa &#42; su obavezna.</p>
                         <form class="py-4">
                             <div class="form-group">
-                                <label for="inputName" class="small text-uppercase">Name *</label>
-                                <input type="text" class="form-control" id="inputName" placeholder="Enter your name" required="">
+                                <label for="inputName" class="small text-uppercase">Ime &#42;</label>
+                                <input v-model.lazy="userInfo.name" type="text" class="form-control" id="inputName" placeholder="Unesite vaše ime" required="">
                             </div>
                             <div class="form-group">
-                                <label for="inputEmail" class="small text-uppercase">Email *</label>
-                                <input type="email" class="form-control" id="inputEmail" placeholder="Enter your email address" required="">
+                                <label for="inputLastname" class="small text-uppercase">Prezime &#42;</label>
+                                <input v-model.lazy="userInfo.lastname" type="text" class="form-control" id="inputLastname" placeholder="Unesite vaše prezime" required="">
                             </div>
                             <div class="form-group">
-                                <label for="messageTetxarea" class="small text-uppercase">Message *</label>
-                                <textarea class="form-control rounded" id="messageTetxarea" rows="9" placeholder="Enter your message" required=""></textarea>
+                                <label for="inputPhone" class="small text-uppercase">Telefon</label>
+                                <input v-model.lazy="userInfo.lastname" type="text" class="form-control" id="inputPhone" placeholder="Unesite vaš broj telefona">
                             </div>
-                            <div class="form-group form-check">
-                                <input type="checkbox" class="form-check-input" id="inputCheck">
-                                <label class="form-check-label small" for="inputCheck">I agree with the <a href="#">terms and conditions</a> </label>
+                            <div class="form-group">
+                                <label for="inputEmail" class="small text-uppercase">Email &#42;</label>
+                                <input v-model.lazy="userInfo.email" type="email" class="form-control" id="inputEmail" placeholder="Unesite email adresu" required="">
                             </div>
-                            <button type="submit" class="btn btn-pill btn-primary">Submit</button>
+                            <div class="form-group">
+                                <label for="messageTetxarea" class="small text-uppercase">Poruka &#42;</label>
+                                <textarea v-model.lazy="message" class="form-control rounded" id="messageTetxarea" rows="9" placeholder="Unesite poruku" required=""></textarea>
+                            </div>
+                            <button type="submit" class="btn btn-pill btn-primary">Pošalji</button>
                         </form>
                     </div>
                 </div>
@@ -34,3 +38,31 @@
         </div><!-- end bg-skew -->
     </main>
 </template>
+
+<script>
+export default {
+    name: 'Contact_View',
+    data() {
+        return {
+           userInfo: {
+               name: '',
+               email: ''
+           },
+           message: ''
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../../assets/scss/settings';
+.disabled {
+  background: $primary !important;
+  pointer-events: none;
+}
+p, label {
+    color: $secondary-text !important;
+}
+</style>
+
+
