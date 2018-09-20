b41db152ebd183166798c2e0f501fe6fbc571c72
VladTzaru <bvanja.87@gmail.com>
2018-09-20 19:42:43 +0200

    add sticky-nav

--- src/components/base/navigation/MainNavigation.vue
+++ src/components/base/navigation/MainNavigation.vue
@@ -1,31 +1,35 @@
 <template>
   <header>
     <div class="container">
-      <nav class="navbar navbar-expand-lg navbar-light">
-        <router-link to="/" tag="a" class="navbar-brand">Royal Logo</router-link>
-        <button @click="isToggled = !isToggled" class="navbar-toggler" type="button" aria-controls="mainNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
+      <div class="nav-wrapper">
+        <nav v-scroll="stickyNav" class="navbar navbar-expand-lg">
+         
+          <router-link to="/" tag="a" class="navbar-brand">Royal Logo</router-link>
+          
+          <button @click="isToggled = !isToggled" class="navbar-toggler" type="button" aria-controls="mainNav"
+            aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
 
-        <div class="collapse navbar-collapse justify-content-end" id="mainNav">
-          <ul class="navbar-nav">
-            <router-link to="/o-nama" active-class="active" tag="li" class="nav-item">
-              <a class="nav-link text-uppercase">O nama</a></router-link>
+          <div class="collapse navbar-collapse justify-content-end">
+            <ul class="navbar-nav">
+              <router-link to="/o-nama" active-class="active" tag="li" class="nav-item">
+                <a class="nav-link text-uppercase">O nama</a></router-link>
 
-            <router-link to="/usluge" active-class="active" tag="li" class="nav-item">
-              <a class="nav-link text-uppercase">Usluge</a></router-link>
+              <router-link to="/usluge" active-class="active" tag="li" class="nav-item">
+                <a class="nav-link text-uppercase">Usluge</a></router-link>
 
-            <router-link to="/kontakt" active-class="active" tag="li" class="nav-item">
-              <a class="nav-link text-uppercase">Kontakt</a></router-link>
-          </ul>
-        </div>
-      </nav>
+              <router-link to="/kontakt" active-class="active" tag="li" class="nav-item">
+                <a class="nav-link text-uppercase">Kontakt</a></router-link>
+            </ul>
+          </div>
+
+        </nav>
+      </div> <!-- .nav-wrapper ends -->
+
     </div>
 
-    <SideNavigation 
-      :show="isToggled"
-      @wasToggled="isToggled = $event"
-      @wasClicked="isToggled = $event"/>
+    <SideNavigation :show="isToggled" @wasToggled="isToggled = $event" @wasClicked="isToggled = $event" />
 
   </header>
 </template>
@@ -41,6 +45,15 @@ export default {
   },
   components: {
     SideNavigation
+  },
+  methods: {
+    stickyNav(evt, el) {
+      if (window.scrollY > 550) {
+        el.classList.add('navbar-sticky');
+      } else {
+        el.classList.remove('navbar-sticky');
+      }
+    }
   }
 }
 </script>
@@ -48,8 +61,10 @@ export default {
 <style lang="scss" scoped>
 @import '../../../assets/scss/settings';
 header {
-  box-shadow: $header-shadow;
   padding: 18px 0;
+}
+.navbar-expand-lg {
+  height: 84.44px;
 }
 .nav-link {
   letter-spacing: 1px;
@@ -61,5 +76,31 @@ header {
   &:hover, &:focus {
     color: $secondary-light !important;
   }
+}
+.nav-wrapper {
+  width: 100%;
+  height: 84.44px;
+}
+
+.navbar-sticky {
+  position: fixed;
+  background-color: #fff;
+  box-shadow: $header-shadow;
+  animation: ease-in-top 300ms ease-in;
+  transition: $transition-primary;
+  width: 100%;
+  z-index: 1000;
+  top: 0;
+  left: 0;
+  transform: translate3d(0, 0, 0);
+}
+
+@keyframes ease-in-top {
+  from {
+    transform: translateY(-84.44px); 
+  }
+  to {
+    transform: translateY(0); 
+  } 
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,19 @@ import App from './App.vue'
 import router from './router'
 import Meta from 'vue-meta'
 
+// DIRECTIVES
+Vue.directive('scroll', {
+  inserted (el, binding) {
+    let f = evt => {
+      if (binding.value(evt, el)) {
+        window.removeEventListener('scroll', f)
+      }
+    }
+    window.addEventListener('scroll', f)
+  }
+});
+
+// META SETUP
 Vue.use(Meta, {
   keyName: 'metaInfo', // the component option name that vue-meta looks for meta info on.
   attribute: 'data-vue-meta', // the attribute name vue-meta adds to the tags it observes
@@ -12,6 +25,7 @@ Vue.use(Meta, {
 
 Vue.config.productionTip = false
 
+// INSTANCE
 new Vue({
   router,
   render: h => h(App),
