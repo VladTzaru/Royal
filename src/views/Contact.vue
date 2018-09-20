bfa7e61c6d95204d6ffd8cfd3be837d89aab449d
VladTzaru <bvanja.87@gmail.com>
2018-09-20 12:54:35 +0200

    add team to contact

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -4,10 +4,16 @@
             <h1 slot="hero-title" class="display-4">Imate pitanje? Kontaktirajte nas.</h1>
             <p slot="hero-text" class="lead mb-4">Trudićemo se da Vam odgovorimo u najkraćem vremenskom roku.</p>
             <img slot="hero-img" src="../assets/img/contact.jpg" class="d-none d-lg-block img-fluid img-thumbnail" alt="Kontakt">
-        </Hero>
+        </Hero> 
         <div class="bg-skew bg-skew-light">
             <Contact/>
-            <Testimonials/>
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-lg-9">
+                        <TeamMembers/>
+                    </div>
+                </div>
+            </div>
         </div>
     </main>
 </template>
@@ -15,7 +21,7 @@
 <script>
 import Hero from '../components/hero/Hero.vue';
 import Contact from '../components/base/Contact.vue';
-import Testimonials from '../components/testimonials/Testimonials.vue';
+import TeamMembers from '../components/team/TeamMembers.vue';
 export default {
     name: 'ContactView',
     metaInfo: {
@@ -29,7 +35,7 @@ export default {
     components: {
         Hero, 
         Contact,
-        Testimonials
+        TeamMembers
     }
 }
 </script>
